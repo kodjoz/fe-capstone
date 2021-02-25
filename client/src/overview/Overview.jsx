e5c31f4546a5a10ff28f1434e9c6334e0eacf5d4
Stephen Hyde <shh2112@gmail.com>
2021-02-25 15:43:08 -0500

    create OverviewContainer component to hold all Overview module child components

---
+++ client/src/overview/Overview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OverviewContainer = () => {
+  return (
+    <div>
+      <h1>Overview Module Placeholder</h1>
+    </div>
+  );
+};
+
+export default OverviewContainer;
