bd044e0b0e416f1cca1c5f62f031685ad973944e
Kaleb Lyda <kaleblyda14@gmail.com>
2021-02-25 15:46:37 -0500

    Imported container components for each widget

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import RelatedProducts from './relatedProducts/RelatedProducts.jsx';
+import ReviewsList from './ratingsAndReviews/ReviewsList.jsx';
+import Overview from './overview/Overview.jsx';
 
 const App = () => {
 
   return (
     <div>
       <h1>Hello, world!</h1>
+      {/* <Overview/> */ }
+      <RelatedProducts/>
+      {/* <ReviewsList/> */}
     </div>
   );
-}
+};
 
 export default App;
 
