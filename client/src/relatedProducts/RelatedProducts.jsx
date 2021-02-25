db3888065ecb275bdf289c6de036be216c200764
Kaleb Lyda <kaleblyda14@gmail.com>
2021-02-25 14:15:38 -0500

    Exporting container component

---
+++ client/src/relatedProducts/RelatedProducts.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const fooButton = styled.button`
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  color: blue;
+`;
+
+const RelatedProducts = () => {
+  return (
+    <div>
+      <fooButton/>
+    </div>
+  );
+};
+
+
+export default RelatedProducts;
