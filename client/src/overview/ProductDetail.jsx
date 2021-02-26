1bf9a7b36d415e54abd630368849f0e7903ad848
Stephen Hyde <shh2112@gmail.com>
2021-02-26 12:44:56 -0500

    create component ProductDetail

---
+++ client/src/overview/ProductDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductDetail = () => {
+  return (
+    <div>
+      <h4>Stars</h4>
+      <h4>Category</h4>
+      <h1>Product Name</h1>
+      <h4>Price</h4>
+    </div>
+  );
+};
+
+export default ProductDetail;
