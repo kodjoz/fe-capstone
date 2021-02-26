70e4eb6280ff62e4a73cdc41fd5e8670dc612488
Stephen Hyde <shh2112@gmail.com>
2021-02-26 12:44:12 -0500

    create component ImageGallery

---
+++ client/src/overview/ImageGallery.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ImagePlaceholder = styled.div`
+  width: 100%;
+  background-color: #ccc;
+  height: 300px;
+`;
+
+const ImageGallery = () => {
+  return (
+    <ImagePlaceholder />
+  );
+};
+
+export default ImageGallery;
