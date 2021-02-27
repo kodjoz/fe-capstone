223cc1a9df3c84c0ac9fead26b13a979fc86cd5e
Stephen Hyde <shh2112@gmail.com>
2021-02-27 14:12:04 -0500

    render slogan and description from passed in data

--- client/src/overview/ProductDescription.jsx
+++ client/src/overview/ProductDescription.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ProductDescription = () => {
+const ProductDescription = (props) => {
+
+  // if no product data is loaded, return empty div
+  if (!props.product) {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+  const slogan = <h3>{props.product.slogan}</h3>;
+  const description = props.product.description ? <p>{props.product.description}</p> : '';
+
   return (
-    <p>Product description</p>
+    <div>
+      {slogan}
+      {description}
+    </div>
   );
+};
+
+ProductDescription.propTypes = {
+  product: PropTypes.shape({
+    slogan: PropTypes.string,
+    description: PropTypes.string
+  })
 };
 
 export default ProductDescription;
