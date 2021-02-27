ab50771140878f3fcb8dbdf60d93bea1477daba9
Kaleb Lyda <kaleblyda14@gmail.com>
2021-02-27 16:51:06 -0500

    Created Modal component

---
+++ client/src/relatedProducts/Modal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Modal = (props) => {
+  var { handleClose, show, children } = props;
+  return (
+    <div className={props.className}>
+      <h3>Comparing</h3>
+      <section>
+        {children}
+        <button type="button" onClick={handleClose}>
+          close
+        </button>
+      </section>
+    </div>
+  );
+};
+
+
+const StyledModal = styled(Modal)`
+  display: ${props => props.show ? 'block' : 'none'};
+  position: fixed;
+  width: 30%;
+  height: auto;
+  top: 50%;
+  left: 50%;
+  background: #f2f2f2;
+`;
+
+export default StyledModal;
