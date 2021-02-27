985dfda827e03d3f4d503b2efcaf35f3a0da1785
Kaleb Lyda <kaleblyda14@gmail.com>
2021-02-27 14:35:20 -0500

    Changed rounding to reflect rating input from 0-100

--- client/src/starRow.jsx
+++ client/src/starRow.jsx
@@ -21,8 +21,8 @@ const StyledStarRow = styled.div`
 
 const StarRow = (props) => {
   var rating = props.rating || 0;
-  //Rounds number to nearest quarter with maximum of 2 digits after decimal
-  rating = (Math.round(rating * 4) / 4).toFixed(2);
+  //Rounds number to nearest quarter
+  rating = Math.round(rating / 25) * 25;
   return (
     <div>
       <StyledStarRow rating={rating || 0} size={props.size || 30}></StyledStarRow>
