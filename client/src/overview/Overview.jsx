1195022ce29619b31a6df26e2d1c233c2212e61d
Stephen Hyde <shh2112@gmail.com>
2021-02-27 12:51:36 -0500

    refactor OverviewContainer as a class

--- client/src/overview/Overview.jsx
+++ client/src/overview/Overview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddToCart from './AddToCart';
 import ImageGallery from './ImageGallery';
 import ProductDescription from './ProductDescription';
@@ -6,37 +7,58 @@ import ProductDetail from './ProductDetail';
 import StyleSelector from './StyleSelector';
 import styled from 'styled-components';
 
-const OverviewContainer = () => {
+class OverviewContainer extends React.Component {
 
-  const Grid = styled.div`
-    border: 1px solid #ddd;
-    display: grid;
-    grid-column-gap: 1rem;
-  `;
+  constructor(props) {
+    super(props);
+    this.state = {
+      product_id: props.product_id,
+      product: null
+    };
+  }
 
-  const LeftContainer = styled.div`
-    grid-column-start: 1;
-    grid-column-end: 2;
-  `;
+  render() {
+    const Grid = styled.div`
+      border: 1px solid #ddd;
+      display: grid;
+      grid-column-gap: 1rem;
+    `;
 
-  const RightContainer = styled.div`
-    grid-column-start: 2;
-    grid-column-end: 3;
-  `;
+    const LeftContainer = styled.div`
+      grid-column-start: 1;
+      grid-column-end: 2;
+    `;
 
-  return (
-    <Grid>
-      <LeftContainer>
-        <ImageGallery />
-      </LeftContainer>
-      <RightContainer>
-        <ProductDetail />
-        <StyleSelector />
-        <AddToCart />
-      </RightContainer>
-      <ProductDescription />
-    </Grid>
-  );
+    const RightContainer = styled.div`
+      grid-column-start: 2;
+      grid-column-end: 3;
+    `;
+
+    return (
+      <Grid>
+        <LeftContainer>
+          <ImageGallery />
+        </LeftContainer>
+        <RightContainer>
+          <ProductDetail />
+          <StyleSelector />
+          <AddToCart />
+        </RightContainer>
+        <ProductDescription />
+      </Grid>
+    );
+  }
+}
+
+OverviewContainer.propTypes = {
+  product_id: PropTypes.number,
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    slogan: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.string
+  })
 };
 
 export default OverviewContainer;
