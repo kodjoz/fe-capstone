894acfba7331ed908af45ff21d3332c490cb748f
Kaleb Lyda <kaleblyda14@gmail.com>
2021-02-27 16:52:58 -0500

    Handles Modal display based on button click

--- client/src/relatedProducts/Slide.jsx
+++ client/src/relatedProducts/Slide.jsx
@@ -2,16 +2,36 @@ import React, { Component } from 'react';
 import StyledSlideInfo from './SlideInfo.jsx';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import StyledModal from './Modal.jsx';
 
 class Slide extends Component {
   constructor(props) {
     super(props);
     this.handleButtonClick = this.handleButtonClick.bind(this);
+
+    this.state = {
+      showModal: false
+    };
+    this.show = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+  }
+
+  showModal() {
+    this.setState({
+      showModal: true
+    });
+  }
+
+  hideModal() {
+    this.setState({
+      showModal: false
+    });
   }
 
   handleButtonClick(e) {
     e.preventDefault();
     console.log('clicked');
+    this.showModal();
   }
 
   render() {
@@ -19,6 +39,9 @@ class Slide extends Component {
       <div className={this.props.className}>
         {this.props.render(this.handleButtonClick)}
         <StyledSlideInfo data={this.props.data}></StyledSlideInfo>
+        <StyledModal show={this.state.showModal} handleClose={this.hideModal}>
+          <p>Modal</p>
+        </StyledModal>
       </div>
     );
   }
