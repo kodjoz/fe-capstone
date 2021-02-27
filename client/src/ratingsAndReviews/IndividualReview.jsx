cb26dfd40f8f46d7d5459f24dd1f5f85a75d7750
Phil Teves <phillip.teves@gmail.com>
2021-02-27 15:39:47 -0500

    One-line review summary now renders, add'l lines also conditionally render separately

--- client/src/ratingsAndReviews/IndividualReview.jsx
+++ client/src/ratingsAndReviews/IndividualReview.jsx
@@ -15,6 +15,7 @@ class IndividualReview extends React.Component {
     this.setState({helpfulRated: true});
   }
   render() {
+    let review = this.props.review;
     // let body;
     // if (props.review.body.length <= 250) {
     //   body = <p>{props.review.body}</p>
@@ -23,17 +24,32 @@ class IndividualReview extends React.Component {
     //   <span onClick={() => {document.getElementById('review-body').value = this.state.reviewBodyExtra}}>Show more...</span>
     //   </p>
     // }
-    let review = this.props.review;
+    review.summary = 'It was the best of shreks, it was the worst of shreks, it was the age of shrekdom, it was the age of shrekishness, it was the epoch of shreklief, it was the epoch of inshrekulity'; //long summary test
+    let summary = review.summary;
+    if (summary.length > 60) {
+      summary = <div><b>{review.summary.slice(0,60)}...</b><p>...{review.summary.slice(60, review.summary.length-1)}</p></div>;
+    } else {
+      summary = <b>{review.summary}</b>;
+    }
+
+    // let body = null;
+    // if (review.body.length > 250) {
+    //   body = <p>{review.body}</p>
+    // } else {
+    //   body = <div><p>{</p></div>
+    // }
+
     let response = null;
     //review.response = 'im a seller and give me all ya monayyyy~';
     if (review.response) {
       response = <p className='seller-response'>{review.response}</p>;
     }
+
     //review.rating = 2.5;
     //review.recommend = false;
     let recommend = null;
     if (review.recommend) {
-      recommend = <span>&#10003; I recommend this product</span>
+      recommend = <span>&#10003; I recommend this product</span>;
     }
     return (
       <div>
@@ -41,7 +57,8 @@ class IndividualReview extends React.Component {
         <StarRow size={15} rating={review.rating * 20} />
         <span>{review.reviewer_name}</span>
         <p>{new Date(review.date).toLocaleString('en-US', {month: 'long', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})}</p>
-        <b>{review.summary} - <em>use ... to truncate me into one line</em></b>
+        <div>{summary}</div>
+        {/* <b>{review.summary} - <em>use ... to truncate me into one line</em></b> */}
         <p>{review.body} - <em>slice me into 250 char, w a <b>Show more</b> link</em></p>
         <p>{recommend}</p>
         <span>{response}</span>
