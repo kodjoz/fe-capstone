dc2bab86d51f68c82be4b4cb71320574263b6171
Kaleb Lyda <kaleblyda14@gmail.com>
2021-02-27 21:01:12 -0500

    Creates outfit-specific event handler

--- client/src/relatedProducts/OutfitCarousel.jsx
+++ client/src/relatedProducts/OutfitCarousel.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import StyledSlide from './Slide.jsx';
+
 
 class OutfitCarousel extends React.Component {
   constructor(props) {
     super(props);
+
+    this.cardButtonClick = this.cardButtonClick.bind(this);
+  }
+
+  cardButtonClick() {
+    console.log('outfit click');
   }
 
   render() {
     return (
       <>
         <div>First Slide</div>
+        {Object.values(this.props.data).map((product) => {
+          return <StyledSlide data={product}
+            cardButtonClick={this.cardButtonClick}
+            key={product.id}
+            render={onClick => (
+              <button onClick={onClick}>x</button>
+            )}>
+          </StyledSlide>;
+        })}
       </>
     );
   }
