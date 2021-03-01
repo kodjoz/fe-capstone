fcb3b49d3beef23cf85c0c06ddd39f734b2cb40e
kalebal <kaleblyda14@gmail.com>
2021-03-01 13:45:16 -0500

    Renamed local storage to avoid name clashes, adds fake data to local storage on add button click

--- client/src/relatedProducts/OutfitCarousel.jsx
+++ client/src/relatedProducts/OutfitCarousel.jsx
@@ -3,22 +3,33 @@ import PropTypes from 'prop-types';
 import StyledSlide from './Slide.jsx';
 import styled from 'styled-components';
 
+let fakeProduct = {'id': 19090, 'campus': 'hr-rfe', 'name': 'Bright Future Sunglasses', 'slogan': 'You\'ve got to wear shades', 'description': 'Where you\'re going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.', 'category': 'Accessories', 'default_price': '69.00', 'created_at': '2021-02-23T19:24:34.450Z', 'updated_at': '2021-02-23T19:24:34.450Z', 'features': [{'feature': 'Lenses', 'value': 'Ultrasheen'}, {'feature': 'UV Protection', 'value': null}, {'feature': 'Frames', 'value': 'LightCompose'}]};
+
 
 class OutfitCarousel extends React.Component {
   constructor(props) {
     super(props);
     this.removeFromOutfit = this.removeFromOutfit.bind(this);
     this.addToOutfit = this.addToOutfit.bind(this);
+    this.addProductClickHandler = this.addProductClickHandler.bind(this);
 
     this.state = {
-      yourOutfit: JSON.parse(
-        window.localStorage.getItem('yourOutfit')
-      ) || {}
+      yourOutfit: {}
     };
   }
 
+  componentDidMount() {
+    var localStorage = JSON.parse(window.localStorage.getItem('relatedProducts'));
+    if (localStorage && localStorage.yourOutfit) {
+      this.setState({
+        yourOutfit: localStorage.yourOutfit
+      });
+    }
+  }
   componentDidUpdate() {
-    window.localStorage.setItem('yourOutfit', JSON.stringify(this.state));
+    console.log(this.state.yourOutfit);
+    window.localStorage.setItem('relatedProducts', JSON.stringify(this.state));
+    //window.localStorage.setItem('relatedProducts', 'hi');
   }
 
   removeFromOutfit() {
@@ -28,11 +39,19 @@ class OutfitCarousel extends React.Component {
     console.log('remove from Outfit');
   }
 
-  addToOutfit() {
+  addToOutfit(newProduct) {
+    let currentOutfit = this.state.yourOutfit;
+    currentOutfit[newProduct['id']] = newProduct;
     this.setState({
-      yourOutfit: 'hi'
+      yourOutfit: currentOutfit
     });
-    console.log('add to outfit');
+  }
+
+  addProductClickHandler(event) {
+    event.preventDefault();
+    //get product data from app
+    //pass to addToOutfit as parameter
+    this.addToOutfit(fakeProduct);
   }
 
   render() {
@@ -40,8 +59,9 @@ class OutfitCarousel extends React.Component {
       <>
         <FirstSlide>
           First Slide
-          <button onClick={this.addToOutfit}>add to outfit</button>
+          <button onClick={this.addProductClickHandler}>add to outfit</button>
         </FirstSlide>
+        {/* change to use this.state.yourOutfit once add button adds productData */}
         {Object.values(this.props.data).map((product) => {
           return <StyledSlide data={product}
             cardButtonClick={this.removeFromOutfit}
@@ -68,5 +88,6 @@ const FirstSlide = styled.div`
   flex-direction: column;
   justify-content: center;
 `;
+
 
 export default OutfitCarousel;
