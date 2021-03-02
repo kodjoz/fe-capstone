33ad34f9786e5fb5cbe97c0aac77e8c4cabb8402
Zach Kodjo <zach.n.kodjo@gmail.com>
2021-03-02 10:45:20 -0500

    ESLint fixes

---
+++ client/src/questionAndAnswer/AddAnswerForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//import axios from 'axios'; // trying to see if I need to import Axios here because it's already imported in parent
+import PropTypes from 'prop-types';
+//import styled from 'styled-components';
+
+class AddAnswer extends React.Component {
+
+
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+AddAnswer.propTypes = {
+  answer: PropTypes.number.isRequired
+};
+
+export default AddAnswer;
