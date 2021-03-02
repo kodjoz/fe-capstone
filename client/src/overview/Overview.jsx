bb19fb05ac641cfec808924d5e0143106005a153
Stephen Hyde <shh2112@gmail.com>
2021-03-02 15:41:06 -0500

    stop duplicating props.product_id in state and move styled constants outside of render method

--- client/src/overview/Overview.jsx
+++ client/src/overview/Overview.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import AddToCart from './AddToCart';
 import ImageGallery from './ImageGallery';
 import ProductDescription from './ProductDescription';
@@ -8,69 +7,43 @@ import ProductDetail from './ProductDetail';
 import StyleSelector from './StyleSelector';
 import styled from 'styled-components';
 
+const Grid = styled.div`
+border: 1px solid #ddd;
+display: grid;
+grid-column-gap: 1rem;
+`;
+
+const LeftContainer = styled.div`
+grid-column-start: 1;
+grid-column-end: 2;
+`;
+
+const RightContainer = styled.div`
+grid-column-start: 2;
+grid-column-end: 3;
+`;
+
 class OverviewContainer extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      product_id: props.product_id,
-      product: null
+      product: props.product
     };
   }
 
-  componentDidMount() {
-    if (this.state.product_id !== undefined) {
-      this.fetchProductData(this.state.product_id)
-        .then((productData) => {
-          this.setState({
-            product: productData
-          });
-        })
-        .catch((error) => {
-          console.log('error fetching data', error);
-        });
-    }
-  }
-
-  fetchProductData(id) {
-    const productUrl = `/api/products/${id}`;
-    return axios.get(productUrl)
-      .then((results) => {
-        if (results && results.data) {
-          return results.data;
-        }
-        throw Error('fetchProductData failed');
-      });
-  }
-
   render() {
-    const Grid = styled.div`
-      border: 1px solid #ddd;
-      display: grid;
-      grid-column-gap: 1rem;
-    `;
-
-    const LeftContainer = styled.div`
-      grid-column-start: 1;
-      grid-column-end: 2;
-    `;
-
-    const RightContainer = styled.div`
-      grid-column-start: 2;
-      grid-column-end: 3;
-    `;
-
     return (
       <Grid>
         <LeftContainer>
           <ImageGallery />
         </LeftContainer>
         <RightContainer>
-          <ProductDetail product={this.state.product} />
+          <ProductDetail product={this.props.product} />
           <StyleSelector />
           <AddToCart />
         </RightContainer>
-        <ProductDescription product={this.state.product}/>
+        <ProductDescription product={this.props.product}/>
       </Grid>
     );
   }
