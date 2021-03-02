5e9ceb362b239a39c23b0c077c85acab54fd6524
Stephen Hyde <shh2112@gmail.com>
2021-03-02 17:29:42 -0500

    create PriceDisplay component

---
+++ client/src/overview/PriceDisplay.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StyleType } from './types.js';
+
+const PriceContainer = styled.p`
+  padding: 0.25rem;
+`;
+
+const StruckSpan = styled.span`
+  text-decoration: line-through;
+`;
+
+const RedSpan = styled.span`
+  color: red;
+`;
+
+const DisplayPrice = styled.span`
+  color: black;
+  font-weight: bold;
+`;
+
+const PriceDisplay = (props) => {
+  if (!props.selectedStyle) {
+    return '';
+  }
+  if (props.selectedStyle.sale_price) {
+    return (
+      <PriceContainer>
+        <StruckSpan>{props.selectedStyle.original_price}</StruckSpan>
+        <RedSpan>{props.selectedStyle.sale_price}</RedSpan>
+      </PriceContainer>
+    );
+  } else {
+    return (
+      <PriceContainer>
+        <DisplayPrice>{props.selectedStyle.original_price}</DisplayPrice>
+      </PriceContainer>
+    );
+  }
+};
+
+PriceDisplay.propTypes = {
+  selectedStyle: StyleType
+};
+
+export default PriceDisplay;
