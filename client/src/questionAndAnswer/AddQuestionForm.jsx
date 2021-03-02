264269d9a10259126b2a104ba649d09dddc6a053
Zach Kodjo <zach.n.kodjo@gmail.com>
2021-03-02 09:41:50 -0500

    Added input validation to form, fixed the state management of the form

--- client/src/questionAndAnswer/AddQuestionForm.jsx
+++ client/src/questionAndAnswer/AddQuestionForm.jsx
@@ -8,62 +8,72 @@ class AddQuestion extends React.Component {
     super(props);
 
     this.state = {
-      name: '',
-      email: '',
-      body: ''
+      form: {},
     };
 
     this.submitQuestion = this.submitQuestion.bind(this);
   }
 
-  submitQuestion() {
-    if (this.state.name === '' || this.state.email === '' || this.state.body === '') {
-      alert('You forgot to fill out a part of this form. Fill out the entire form before submitting');
-    } else {
-      return axios.post('/api/qa/question', {
-        data: {
-          name: this.state.name,
-          email: this.state.email,
-          body: this.state.body,
-          product_id: this.props.product_id
-        }
-      })
-        .then(() => {
-          this.setState({
-            name: '',
-            email: '',
-            body: '',
-          });
+  handleQuestionInput(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      form: {
+        [name]: value
+      }
+    });
+  }
+
+  submitQuestion(e) {
+    e.preventDefault();
+    return axios.post('/api/qa/question', {
+      data: this.state.form
+    })
+      .then(() => {
+        this.setState({
+          form: {}
         });
-    }
+      });
   }
 
   render() {
     return (
-      <AddQuestionForm>
-        <input type="text" required
-          name="name"
-          placeholder="Enter your name"
-          value={this.state.name}
-          onChange={(e) => this.setState({ name: e.target.value })}/><br />
-        <input
-          type="text" required
-          name="email"
-          placeholder="Enter your email"
-          value={this.state.email}
-          onChange={(e) => this.setState({ email: e.target.value })}/><br />
-        <textarea
-          type="text" required
-          name="body"
-          placeholder="Add your question"
-          value={this.state.body}
-          onChange={(e) => this.setState({ body: e.target.value })}>
-        </textarea><br />
+      <AddQuestionForm onSubmit={this.submitQuestion}>
+        <label>Nickname:
+          <input type="text" required
+            name="name"
+            maxLength="60"
+            placeholder="Enter your name"
+            onChange={(e) => this.handleQuestionInput(e)}
+          /></label>
+        <br />
+        <label>Email:
+          <input
+            type="email" required
+            name="email"
+            maxLength="60"
+            placeholder="Enter your email"
+            onChange={(e) => this.handleQuestionInput(e)}
+          />
+        </label>
+        <br />
+        <label>Question:
+          <textarea
+            type="text" required
+            name="body"
+            maxLength="1000"
+            cols="50"
+            rows="4"
+            placeholder="Add your question"
+            onChange={(e) => this.handleQuestionInput(e)}
+          />
+        </label>
+        <br />
         <input
           type="submit"
           name="Submit"
           value="Submit"
-          onClick={this.submitQuestion}/>
+        />
       </AddQuestionForm>
     );
   }
@@ -75,7 +85,7 @@ const AddQuestionForm = styled.form`
 `;
 
 AddQuestion.propTypes = {
-  product_id: PropTypes.number.isRequired
+  product: PropTypes.number.isRequired
 };
 
 export default AddQuestion;
