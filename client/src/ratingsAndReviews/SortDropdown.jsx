566b73a82b8a07229fb9c45e506087f23a6e4eea
Phil Teves <phillip.teves@gmail.com>
2021-03-03 13:31:15 -0500

    fixed newline

--- client/src/ratingsAndReviews/SortDropdown.jsx
+++ client/src/ratingsAndReviews/SortDropdown.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, DropdownMenu } from '../globalStyles.js';
+import { DropdownMenu } from '../globalStyles.js';
 
 const SortDropdown = () => {
   return (
