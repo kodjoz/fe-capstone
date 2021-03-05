409fd4a2f1dab55ab79f522084ee7a332f206ea9
Phil Teves <phillip.teves@gmail.com>
2021-03-04 19:13:55 -0500

    factor bar now utilizes a more consistent grid layout

--- client/src/ratingsAndReviews/FactorBar.jsx
+++ client/src/ratingsAndReviews/FactorBar.jsx
@@ -10,9 +10,9 @@ const FactorBar = (props) => {
       <Indicator location={props.average}>&#9662;</Indicator>
       <FullBar><Bar></Bar><Bar></Bar><Bar></Bar></FullBar>
       <Values>
-        <Value>{props.range[0]}</Value>
-        <Value>{props.range[1]}</Value>
-        <Value>{props.range[2]}</Value>
+        <Value1>{props.range[0]}</Value1>
+        <Value2>{props.range[1]}</Value2>
+        <Value3>{props.range[2]}</Value3>
       </Values>
     </Factor>
   );
@@ -54,19 +54,49 @@ const Bar = styled.div`
   display: flex;
 `;
 
+// const Values = styled.div`
+//   width: 100%;
+//   padding: 0 2px;
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+// `;
+// const Value = styled.span`
+//   font-size: 0.8em;
+//   color: hsl(0, 0%, 40%);
+//   font-style: italic;
+// `;
+
 const Values = styled.div`
   width: 100%;
   padding: 0 2px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  display: grid;
 `;
 const Value = styled.span`
   font-size: 0.8em;
   color: hsl(0, 0%, 40%);
   font-style: italic;
+  text-align: center;
 `;
 
+const Value1 = styled(Value)`
+  grid-row-start: 1;
+  grid-row-end: 1;
+  grid-column-start: 1;
+  grid-column-end: 1;
+`;
+const Value2 = styled(Value)`
+  grid-row-start: 1;
+  grid-row-end: 1;
+  grid-column-start: 2;
+  grid-column-end: 2;
+`;
+const Value3 = styled(Value)`
+  grid-row-start: 1;
+  grid-row-end: 1;
+  grid-column-start: 3;
+  grid-column-end: 3;
+`;
 FactorBar.propTypes = {
   name: PropTypes.string,
   range: PropTypes.array,
