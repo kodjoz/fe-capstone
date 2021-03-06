6172ef28330be71ebb0c8f19903c3e9f3eac7635
Stephen Hyde <shh2112@gmail.com>
2021-03-06 13:56:09 -0500

    create test file for Overview module component

---
+++ client/src/overview/Overview.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { setupServer } from 'msw/node';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Overview from './Overview';
+import { MemoryRouter } from 'react-router-dom';
+import handlers from './../mocks/products';
+
+const productData = require('../mocks/products/19089.json');
+
+// import all the mock handlers we created
+const server = setupServer(
+  ...handlers
+);
+
+beforeAll(() => {
+  // start the server before the tests run
+  server.listen({
+    // error if there is a network request that isn't covered by the mocks
+    onUnhandledRequest: 'error'
+  });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+it('renders the Overview module', async () => {
+  render(
+    <MemoryRouter>
+      <Overview product_id={19089} product={productData} />
+    </MemoryRouter>
+  );
+  await waitFor(() => {
+    expect(screen.getByText('Camo Onesie')).toBeInTheDocument();
+  });
+});
