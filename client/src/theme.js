bfc81602b42167f218e26bcc47aaa20724ab5c4a
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-06 21:51:23 -0500

    Add background colors for layering and type colors

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,13 +1,27 @@
 export const lightTheme = {
-  body: '#E2E2E2',
-  text: '#363537',
-  toggleBorder: '#FFF',
-  gradient: 'linear-gradient(#39598A, #79D7ED)',
+  primary: '#CF6679',
+  secondary: '#008786',
+  background: '#F0F0F0',
+  midLayer: '#F5F5F5',
+  midLight: '#F8F8F8',
+  topLayer: '#FFF',
+  error: '#CF6679',
+  primaryText: '#000',
+  secondaryText: '#8A0000',
+  lowPriorityText: '#610000',
+  borders: '#1F000000'
 };
 
 export const darkTheme = {
-  body: '#363537',
-  text: '#FAFAFA',
-  toggleBorder: '#6B8096',
-  gradient: 'linear-gradient(#091236, #1E215D)',
+  primary: '#BB86FC', //light purple
+  secondary: '#03DAC6', //tealish green
+  background: '#121212', //dark grey
+  midLayer: '#202020', //slightly less dark grey
+  midLight: '#303030',
+  topLayer: '#424242',
+  error: '#CF6679', //cool red color
+  primaryText: '#F8F8F8', //white
+  secondaryText: '#D3D3D3', //slightly less white
+  lowPriorityText: '#C0C0C0', //even less white
+  borders: '#F8F8F8'
 };
