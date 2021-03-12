e8bfe2ba42dda8c166539b66376d241d31c14a7d
Stephen Hyde <shh2112@gmail.com>
2021-03-12 13:19:29 -0500

    move key paths

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const https = require('https');
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/zaphcast.com/privkey.pem', 'utf8');
+const privateKey = fs.readFileSync('/home/ubuntu/privkey.pem', 'utf8');
 // const certificate = fs.readFileSync('/etc/letsencrypt/live/zaphcast.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/zaphcast.com/chain.pem', 'utf8');
+const ca = fs.readFileSync('/home/ubuntu/chain.pem', 'utf8');
 
 app.listen(port, () => {
   console.log(`Atelier client is listening on ${port}`);
