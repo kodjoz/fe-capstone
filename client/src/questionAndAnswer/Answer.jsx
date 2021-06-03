9041f9ab053ff6e8302f6fdbe86b1ca95e0b6be9
Zach Kodjo <zach.n.kodjo@gmail.com>
2021-06-03 10:19:24 -0400

    ESLint fixes

--- client/src/questionAndAnswer/Answer.jsx
+++ client/src/questionAndAnswer/Answer.jsx
@@ -1,71 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import ModalImage from '../ModalImage.jsx';
 import { Gallery, Helpful, HelpfulYes } from '../globalStyles.js';
 
-class Answer extends React.Component {
-  constructor(props) {
-    super(props);
+const Answer = (props) => {
+  const [isHelpful, setHelpful] = useState(false);
+  const [isReported, setReported] = useState(false);
 
-    this.state = {
-      isHelpful: false,
-      isReported: false,
-    };
-  }
-
-  handleHelpfulOrReport(endpoint) {
-
-    if (this.state.isHelpful && endpoint === 'helpful') {
+  const handleHelpfulOrReport = (endpoint) => {
+    if (isHelpful && endpoint === 'helpful') {
       alert('You already marked this answer as helpful!');
-    } else if (this.state.isReported && endpoint === 'report') {
+    } else if (isReported && endpoint === 'report') {
       alert('You already reported this answer!');
     } else {
-      this.props.markOrReport('answers', this.props.answer.answer_id, endpoint);
+      props.markOrReport('answers', props.answer.answer_id, endpoint);
+
       if (endpoint === 'helpful') {
-        this.setState({ isHelpful: true });
+        setHelpful(true);
       } else if (endpoint === 'report') {
-        this.setState({ isReported: true });
+        setReported(true);
       }
-      this.props.answer.helpfulness += 1;
+      props.answer.helpfulness += 1;
     }
-  }
+  };
 
-  render() {
-    if (!this.props.answer) {
-      return '';
-    }
+  let answer = props.answer ? props.answer : null;
+  // Options needed to pretty-print the date
+  let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  // pretty print the date
+  let prettyDate = new Date(answer.date).toLocaleDateString('en-US', options);
 
-    let answer = this.props.answer;
-    // Options needed to pretty-print the date
-    let options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    };
-    // pretty print the date
-    let prettyDate = new Date(answer.date).toLocaleDateString('en-US', options);
+  return (
+    <AnswerWrapper>
+      <AnswerSummary><strong>A: </strong>{answer.body}</AnswerSummary>
+      <Gallery>
+        {answer.photos.length !== 0 ? answer.photos.map((photo) => {
+          return (
+            <ModalImage src={photo.url} key={photo.id} />
+          );
+        }) : null}
+      </Gallery>
+      <AnswerLinks>by {answer.answerer_name}, <time>{prettyDate}</time> | <Helpful>Helpful? </Helpful> <HelpfulLinkPreset
+        onClick={() => handleHelpfulOrReport('helpful')}
+        underline={isHelpful ? 'none' : 'underline'}>Yes ({answer.helpfulness})</HelpfulLinkPreset> | <HelpfulLinkPreset
+        onClick={() => this.handleHelpfulOrReport('report')}
+        underline={isReported ? 'none' : 'underline'}>{!isReported ? 'Report' : 'Reported!'}</HelpfulLinkPreset></AnswerLinks>
+    </AnswerWrapper>
+  );
+};
 
-    return (
-      <AnswerWrapper>
-        <AnswerSummary><strong>A: </strong>{answer.body}</AnswerSummary>
-        <Gallery>
-          {answer.photos.length !== 0 ? answer.photos.map((photo) => {
-            return (
-              <ModalImage src={photo.url} key={photo.id} />
-            );
-          }) : null}
-        </Gallery>
-        <AnswerLinks>by {answer.answerer_name}, <time>{prettyDate}</time> | <Helpful>Helpful? </Helpful> <HelpfulLinkPreset
-          onClick={() => this.handleHelpfulOrReport('helpful')}
-          underline={this.state.isHelpful ? 'none' : 'underline'}>Yes ({answer.helpfulness})</HelpfulLinkPreset> | <HelpfulLinkPreset
-          onClick={() => this.handleHelpfulOrReport('report')}
-          underline={this.state.isReported ? 'none' : 'underline'}>{!this.state.isReported ? 'Report' : 'Reported!'}</HelpfulLinkPreset></AnswerLinks>
-      </AnswerWrapper>
-    );
-  }
-}
+
+
+// class Answer extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       isHelpful: false,
+//       isReported: false,
+//     };
+//   }
+
+//   handleHelpfulOrReport(endpoint) {
+
+//     if (this.state.isHelpful && endpoint === 'helpful') {
+//       alert('You already marked this answer as helpful!');
+//     } else if (this.state.isReported && endpoint === 'report') {
+//       alert('You already reported this answer!');
+//     } else {
+//       this.props.markOrReport('answers', this.props.answer.answer_id, endpoint);
+//       if (endpoint === 'helpful') {
+//         this.setState({ isHelpful: true });
+//       } else if (endpoint === 'report') {
+//         this.setState({ isReported: true });
+//       }
+//       this.props.answer.helpfulness += 1;
+//     }
+//   }
+
+//   render() {
+//     if (!this.props.answer) {
+//       return '';
+//     }
+
+//     let answer = this.props.answer;
+//     // Options needed to pretty-print the date
+//     let options = {
+//       year: 'numeric',
+//       month: 'long',
+//       day: 'numeric'
+//     };
+//     // pretty print the date
+//     let prettyDate = new Date(answer.date).toLocaleDateString('en-US', options);
+
+//     return (
+//       <AnswerWrapper>
+//         <AnswerSummary><strong>A: </strong>{answer.body}</AnswerSummary>
+//         <Gallery>
+//           {answer.photos.length !== 0 ? answer.photos.map((photo) => {
+//             return (
+//               <ModalImage src={photo.url} key={photo.id} />
+//             );
+//           }) : null}
+//         </Gallery>
+//         <AnswerLinks>by {answer.answerer_name}, <time>{prettyDate}</time> | <Helpful>Helpful? </Helpful> <HelpfulLinkPreset
+//           onClick={() => this.handleHelpfulOrReport('helpful')}
+//           underline={this.state.isHelpful ? 'none' : 'underline'}>Yes ({answer.helpfulness})</HelpfulLinkPreset> | <HelpfulLinkPreset
+//           onClick={() => this.handleHelpfulOrReport('report')}
+//           underline={this.state.isReported ? 'none' : 'underline'}>{!this.state.isReported ? 'Report' : 'Reported!'}</HelpfulLinkPreset></AnswerLinks>
+//       </AnswerWrapper>
+//     );
+//   }
+// }
 
 // each answer should be an object
 
